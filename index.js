6a03fafe583fa66b29c5c7a02935300c61cc867e
Sam Neale <sammneale@icloud.com>
2021-08-22 14:54:36 +1000

    hub init

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 /* Main Ground Facilities script*/
 // @ts-check
+console.log("IKSA Telem Hub")
 
 import KRPC from './node_modules/krpc.js/lib/KRPC.js';
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+// @ts-ignore
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 //KRPC
 
@@ -15,10 +22,34 @@ const options = {
 
 const krpc = new KRPC(options);
 
+const vehicle = {
+
+}
+
+const gameData = {
+
+}
+
 const telem = {
 
 }
 
+async function init(){
+    try{
+    await krpc.load();
+    }catch(err){
+        console.error("Error, couldn't connect to KSP")
+        fs.writeFileSync(`${__dirname}/errorLog.log`, err.toString())
+        console.error(`A log of this error has been saved at ${__dirname}/errorLog.log`)
+        return;
+    }
+    console.log("Connected to KSP")
+    gameData.sc = krpc.services.spaceCenter;
+    gameData.vessel = await gameData.sc.activeVessel;
+    vehicle.object = gameData.vessel;
+    gameData.control = await gameData.vessel.control;
+}
+init()
 
 //Express
 let auth = "IKSA_AUTH_rg7euhgahiuo"
@@ -34,4 +65,6 @@ expressApp.get("/telem", (req,res) =>{
         res.sendStatus(401);
     }
 })
-expressApp.listen(3001);
+expressApp.listen(3001, ()=>{
+    console.log("IKSA Telemetry Hub listening on 30001");
+});
